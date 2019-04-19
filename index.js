3725dd81e68d87870d72377165cebfe776aefd4b
Reevin <Reevalot@hotmail.com>
2019-04-18 21:01:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import Paddle from '/src/paddle.js';
-import InputHandler from '/src/input.js';
+import Game from '/src/game.js'
 
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
@@ -7,9 +6,8 @@ let ctx = canvas.getContext("2d");
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
-let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
-
-new InputHandler(paddle);
+let game = new Game(GAME_WIDTH, GAME_HEIGHT);
+game.start();
 
 let lastTime = 0;
 
@@ -18,12 +16,13 @@ function gameLoop(timestamp) {
 	let deltaTime = timestamp - lastTime;
 	lastTime = timestamp
 
-	ctx.clearRect(0, 0, 800, 800);
-	paddle.update(deltaTime);
-	paddle.draw(ctx);
+	ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+	
+	game.update(deltaTime);
+	game.draw(ctx);
 
 	requestAnimationFrame(gameLoop);
 
 }
 
-gameLoop();
+requestAnimationFrame(gameLoop);
